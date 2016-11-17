2e94b01bbf293c2874b4e5beabc777620da404ea
Miroslav Vitanov <mvvitanov@gmail.com>
2016-11-17 17:19:35 +0200

    Change the filters presentation

--- resources/views/vendor/administr/filters/between.blade.php
+++ resources/views/vendor/administr/filters/between.blade.php
@@ -1,3 +1,11 @@
-<div class="administr-filters-group">
-    {!! $builder->render() !!}
+<div class="row">
+    <div class="col-sm-12">
+        <label>{{ $field->getLabel() }}</label>
+    </div>
+
+    @foreach($builder->fields() as $field)
+        <div class="col-sm-6">
+            {!! $field->render() !!}
+        </div>
+    @endforeach
 </div>
--- resources/views/vendor/administr/filters/filters.blade.php
+++ resources/views/vendor/administr/filters/filters.blade.php
@@ -1,8 +1,21 @@
 <div class="administr-filters">
-    {!! $builder->render() !!}
-
-    {!! $filterBtn->render() !!}
-    <a href="{{ $clearBtn['url'] }}">
-        {{ $clearBtn['label'] }}
-    </a>
+    <form>
+        @foreach(collect($builder->fields())->chunk(3) as $fields)
+            <div class="row">
+                @foreach($fields as $field)
+                    <div class="col-sm-4">
+                        {!! $field->render() !!}
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+        <div class="row">
+            <div class="col-sm-12">
+                {!! $filterBtn->render() !!}
+                <a href="{{ $clearBtn['url'] }}" class="btn btn-default">
+                    {{ $clearBtn['label'] }}
+                </a>
+            </div>
+        </div>
+    </form>
 </div>
